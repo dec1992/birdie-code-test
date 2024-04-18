1af681d3504b26a70acacff863eb0e6a3b8b6d99
Dec Burns <dec@decs-mbp.home>
2024-04-18 16:16:12 +0100

    lanternfish

--- src/lanternfish/lanternfish.ts
+++ src/lanternfish/lanternfish.ts
@@ -1,1 +1,19 @@
 const initialState = [3, 4, 3, 1, 2];
+
+const calculate = (init: number[], days: number) => {
+  let state: number[] = init;
+  for (let i = 0; i < days; i++) {
+    const newState: number[] = [];
+    state.forEach((i) => {
+      const shouldReset = i - 1 < 0;
+      if (shouldReset) {
+        newState.push(8);
+      }
+      newState.push(!shouldReset ? i - 1 : 6);
+    });
+    state = newState;
+  }
+  return state.length;
+};
+
+console.log(calculate(initialState, 80));
